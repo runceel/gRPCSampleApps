0ccc9a915a5fdd2a873198fec41de70a60152cb8
Kazuki Ota <kaota@microsoft.com>
2019-09-05 14:02:13 +0900

    Add project files.

---
+++ GrpcServiceApp/Services/EmployeeService.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using GrpcSampleApp;
+
+namespace GrpcServiceApp.Services
+{
+    public class EmployeeService : GrpcSampleApp.EmployeeService.EmployeeServiceBase
+    {
+        private static List<Employee> Employees { get; } = new List<Employee>();
+        public override Task<GetEmployeeReply> GetEmployees(Empty request, ServerCallContext context)
+        {
+            lock(Employees)
+            {
+                return Task.FromResult(new GetEmployeeReply
+                {
+                    Employees = { Employees }
+                });
+            }
+        }
+
+        public override Task<AddEmployeeReply> AddEmployee(Employee request, ServerCallContext context)
+        {
+            lock(Employees)
+            {
+                request.Id = Guid.NewGuid().ToString();
+                Employees.Add(request);
+                return Task.FromResult(new AddEmployeeReply
+                {
+                    Succeed = true,
+                });
+            }
+        }
+
+        public override Task<DeleteEmployeeReply> DeleteEmployee(DeleteEmployeeRequest request, ServerCallContext context)
+        {
+            lock(Employees)
+            {
+                var target = Employees.FirstOrDefault(x => x.Id == request.Id);
+                if (target is null)
+                {
+                    return Task.FromResult(new DeleteEmployeeReply
+                    {
+                        Succeed = false,
+                    });
+                }
+
+                Employees.Remove(target);
+                return Task.FromResult(new DeleteEmployeeReply
+                {
+                    Succeed = true,
+                });
+            }
+        }
+    }
+}
